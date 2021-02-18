84d42fb9fb1afea1d1430f855eb8b247d3118014
Janz Clinston <janzclinston@noreply.github.com>
2021-02-18 20:30:52 +0530

    login and sign up updated

--- app/login.php
+++ app/login.php
@@ -1,3 +1,6 @@
+<?php
+session_start();  
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,8 +12,42 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
+  
+  <!-- bootstrab and javascript libraries for tokenfield tags -->
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css"> 
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/css/bootstrap-tokenfield.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/bootstrap-tokenfield.js"></script>
+  
 </head>
 <body>
+<?php
+
+if(isset($_SESSION['success'])){
+   if(isset($_SESSION['error']) && !empty($_SESSION['error'])){
+    ?>
+        <div class="alert alert-success" align="center">
+            <strong><?php echo $_SESSION['error']; ?></strong>
+        </div>
+    <?php
+        unset($_SESSION['success']);
+        unset($_SESSION['error']);
+    } 
+}else{
+
+    if(isset($_SESSION['error']) && !empty($_SESSION['error'])){
+    ?>
+        <div class="alert alert-danger" align="center">
+        	<strong><?php echo $_SESSION['error'];?></strong>
+        </div>
+    <?php
+        unset($_SESSION['error']);
+    }
+}  
+?>
+
 <div class="container" style="padding-top: 100px">
 <div class="row">
 <div class="col-sm-4"></div>
@@ -41,10 +78,10 @@
 	                
 			    </div>
 			    <div id="signUp" class="tab-pane fade">
-			       <form method="POST" action="#" style="padding-top: 80px; margin-left: 25px; margin-right: 25px">
+			       <form method="POST" action="registerUser.php" style="padding-top: 80px; margin-left: 25px; margin-right: 25px">
 			       		<div class="form-group">
-	                    <label for="title">Tittle</label>
-	                    <select class="form-control" id="title" name="title">
+	                    <label for="title">Title</label>
+	                    <select class="form-control" id="title" name="title" required="">
 	                        <option>Mr.</option>
 	                        <option>Mrs.</option>
 	                        <option>Miss.</option>
@@ -57,15 +94,11 @@
 	                    </div>
 	                    <div class="form-group">
 	                    	<label for="lname">Last Name</label>
-	                    	<input type="text" class="form-control" id="lname" name="lname" placeholder="Enter last name"  required="">
+	                    	<input type="text" class="form-control" id="lname" name="lname" placeholder="Enter last name" required="">
 	                    </div>
 	                    <div class="form-group">
 	                    	<label for="dob">DOB</label>
 	                   		<input type="date" class="form-control" id="dob" name="dob" placeholder="DOB" required="">
-	                    </div>
-	                    <div class="form-group">
-	                    	<label for="occupation">Occupation</label>
-	                   		<input type="text" class="form-control" id="occupation" name="occupation" placeholder="Enter Occupation" required="">
 	                    </div>
 	                    <div class="form-group">
 	                    	<label for="nic">NIC</label>
@@ -85,7 +118,22 @@
 	                    </div>
 	                    <div class="form-group">
 	                    	<label for="cpassword">Confirm Password</label>
-	                   		<input type="password" class="form-control" id="cpassword" name="cpassword" placeholder="Re-enter your password" required="">
+	                   		<input type="password" class="form-control" id="cpassword" name="cpassword" placeholder="Re-enter your password" required=""> 
+	                    </div>
+	                    <div class="form-group">
+	                    	<label>Personality Description</label>
+	                   		<input type="text" class="form-control" name="favouriteActivity" id="favouriteActivity" placeholder="Favourite Activity" required="">
+	                   		</br>
+
+		                    <select class="form-control" id="personalityType" name="personalityType" required="">
+		                    	<option value="" disabled selected hidden>Select Personality Type</option>
+		                        <option>Driver</option>
+		                        <option>Analytical</option>
+		                        <option>Amiable</option>
+		                        <option>Expressive</option>
+		                    </select>
+		                	</br>
+		                    <input type="text" name="hobies" id="hobies" class="form-control" placeholder="Hobbies" required=""/>
 	                    </div>
 	                    <div>
 	                   		<input type="checkbox" value="" required/> Agree, Terms and Conditions
@@ -102,5 +150,29 @@
 <div class="col-sm-4"></div>
 </div> 
 </div>
+
+
+<footer class="container">
+    <p>&copy; 2021 Sussex Companions. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</footer>
+
+<script type="text/javascript">
+$(document).ready(function(){
+ $('#hobies').tokenfield({
+  autocomplete:{
+  	source: function (request, response) {
+          jQuery.get("getHobies.php", {
+              query: request.term
+          }, function (data) {
+              data = $.parseJSON(data);
+              response(data);
+          });
+      },
+   	delay:100
+  },
+  showAutocompleteOnFocus: true
+ });
+});
+</script>
 </body>
 </html>
